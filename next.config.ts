b2fd781e720932dcd0b74b8fa8eb08b14eb7f0b0
Keerthana-Ramisetty <keerthanaramisetty972@gmail.com>
2026-05-30 15:03:40 +0100

    modify 7

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,19 @@ import type { NextConfig } from "next";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
-
-
 const nextConfig: NextConfig = {
   images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "shoppydeals.onrender.com",
-      pathname: "/uploads/**",
-    },
-    {
-      protocol: "http",
-      hostname: "localhost",
-      port: "5000",
-      pathname: "/uploads/**",
-    },
-    {
-      protocol: "https",
-      hostname: "images.unsplash.com",
-      pathname: "/**",
-    },
-  ],
-},
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "shoppydeals.onrender.com",
+      },
+    ],
+  },
 
   async rewrites() {
     return [
